d5e529c0e42de0574c9328453bf2b876e779ec64
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2021-11-30 13:57:56 -0500

    prepare 1.2.1 release (#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 import java.time.Duration
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.external.javadoc.CoreJavadocOptions
 
 // These values come from gradle.properties
 val ossrhUsername: String by project
@@ -13,16 +11,9 @@ buildscript {
     }
 }
 
-plugins {
-    java
-    "java-library"
-    checkstyle
-    signing
-    "maven-publish"
-    idea
-    jacoco
-    id("de.marcphilipp.nexus-publish") version "0.3.0"
-    id("io.codearte.nexus-staging") version "0.30.0"
+plugins {  // see Dependencies.kt in buildSrc
+    Libs.javaBuiltInGradlePlugins.forEach { id(it) }
+    Libs.javaExtGradlePlugins.forEach { (n, v) -> id(n) version v }
 }
 
 repositories {
@@ -32,11 +23,30 @@ repositories {
     mavenCentral()
 }
 
-apply { from("build-shared.gradle") }
+configurations.all {
+    // check for updates every build for dependencies with: 'changing: true'
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+}
+
+base {
+    group = ProjectValues.groupId
+    archivesBaseName = ProjectValues.artifactId
+    version = version
+}
 
 java {
     withJavadocJar()
     withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+// See Dependencies.kt in buildSrc for the purpose of "privateImplementation"
+val privateImplementation by configurations.creating
+
+dependencies {  // see Dependencies.kt in buildSrc
+    Libs.privateImplementation.forEach { privateImplementation(it)}
+    Libs.javaTestImplementation.forEach { testImplementation(it) }
 }
 
 checkstyle {
@@ -44,125 +54,38 @@ checkstyle {
 }
 
 tasks.compileJava {
-    // See note in build-shared.gradle on the purpose of "privateImplementation"
+    // See note in Dependencies.kt in buildSrc on "privateImplementation"
     classpath = configurations["privateImplementation"]
 }
 
-tasks.javadoc.configure {
-    // Force the Javadoc build to fail if there are any Javadoc warnings. See: https://discuss.gradle.org/t/javadoc-fail-on-warning/18141/3
-    // See JDK-8200363 (https://bugs.openjdk.java.net/browse/JDK-8200363)
-    // for information about the -Xwerror option.
-    (options as CoreJavadocOptions).addStringOption("Xwerror")
+helpers.Javadoc.configureTask(tasks.javadoc, configurations["privateImplementation"])  // see Javadoc.kt in buildSrc
 
-    // See note in build-shared.gradle on the purpose of "privateImplementation"
-    classpath = configurations["privateImplementation"]
-}
+helpers.Test.configureTask(tasks.compileTestJava, tasks.test,
+    configurations["privateImplementation"])  // see Test.kt in buildSrc
 
-tasks.test.configure {
-    testLogging {
-        events("passed", "skipped", "failed", "standardOut", "standardError")
-        showStandardStreams = true
-        exceptionFormat = TestExceptionFormat.FULL
-    }
-}
+helpers.Jacoco.configureTasks(  // see Jacoco.kt in buildSrc
+    tasks.jacocoTestReport,
+    tasks.jacocoTestCoverageVerification
+)
 
-tasks.jacocoTestReport.configure {
-    reports {
-        xml.isEnabled = true
-        csv.isEnabled = true
-        html.isEnabled = true
-    }
-}
-
-tasks.jacocoTestCoverageVerification.configure {
-    // See notes in CONTRIBUTING.md on code coverage. Unfortunately we can't configure line-by-line code
-    // coverage overrides within the source code itself, because Jacoco operates on bytecode.
-    violationRules {
-        val knownMissedLinesForMethods = mapOf(
-            "EvaluationReason.error(com.launchdarkly.sdk.EvaluationReason.ErrorKind)" to 1,
-            "EvaluationReasonTypeAdapter.parse(com.google.gson.stream.JsonReader)" to 1,
-            "LDValue.equals(java.lang.Object)" to 1,
-            "LDValueTypeAdapter.read(com.google.gson.stream.JsonReader)" to 1,
-            "json.JsonSerialization.getDeserializableClasses()" to -1,
-            "json.LDGson.LDTypeAdapter.write(com.google.gson.stream.JsonWriter, java.lang.Object)" to 1,
-            "json.LDJackson.GsonReaderToJacksonParserAdapter.peekInternal()" to 3
-        )
-
-        knownMissedLinesForMethods.forEach { (signature, maxMissedLines) ->
-            if (maxMissedLines > 0) {  // < 0 means skip entire method
-                rule {
-                    element = "METHOD"
-                    includes = listOf("com.launchdarkly.sdk." + signature)
-                    limit {
-                        counter = "LINE"
-                        value = "MISSEDCOUNT"
-                        maximum = maxMissedLines.toBigDecimal()
-                    }
-                }
-            }
-        }
-        
-        // General rule that we should expect 100% test coverage; exclude any methods that have overrides above
-        rule {
-            element = "METHOD"
-            limit {
-                counter = "LINE"
-                value = "MISSEDCOUNT"
-                maximum = 0.toBigDecimal()
-            }
-            excludes = knownMissedLinesForMethods.map { (signature, maxMissedLines) ->
-                "com.launchdarkly.sdk." + signature }
-        }
-    }
-}
-
-idea {
-    module {
-        isDownloadJavadoc = true
-        isDownloadSources = true
-    }
-}
-
-nexusStaging {
-    packageGroup = "com.launchdarkly"
-    numberOfRetries = 40 // we've seen extremely long delays in closing repositories
-}
+helpers.Idea.configure(idea)
 
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
 
-            groupId = "com.launchdarkly"
-            artifactId = "launchdarkly-java-sdk-common"
-
-            pom {
-                name.set("launchdarkly-java-sdk-common")
-                description.set("LaunchDarkly SDK Java Common Classes")
-                url.set("https://github.com/launchdarkly/java-sdk-common")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("LaunchDarkly")
-                        email.set("team@launchdarkly.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/launchdarkly/java-sdk-common.git")
-                    developerConnection.set("scm:git:ssh:git@github.com:launchdarkly/java-sdk-common.git")
-                    url.set("https://github.com/launchdarkly/java-sdk-common")
-                }
-            }
+            helpers.Pom.standardPom(pom)  // see Pom.kt in buildSrc
         }
     }
     repositories {
         mavenLocal()
     }
+}
+
+nexusStaging {
+    packageGroup = ProjectValues.groupId
+    numberOfRetries = 40 // we've seen extremely long delays in closing repositories
 }
 
 nexusPublishing {
